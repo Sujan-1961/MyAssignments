b0249a12d59b44c4448ec324096658422c90e7e0
Sujan-1961 <123614715+Sujan-1961@users.noreply.github.com>
2023-02-22 12:12:51 +0530

    Add files via upload

---
+++ org/system/Computer.java
@@ -0,0 +1,7 @@
+package org.system;
+
+public class Computer {
+           public void computerModel(String modelName) {
+        	   System.out.println("The Computer model is " + modelName);
+           }
+}
